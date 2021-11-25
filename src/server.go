8d7b08e0c3d3d1ac1c7e12e6e10646bd2b962716
JinAru16 <70217158+JinAru16@users.noreply.github.com>
2021-11-25 16:46:40 +0900

    dd

--- src/server.go
+++ src/server.go
@@ -1,17 +1,10 @@
 package main
-
-import (
-	"log"
-
-	"github.com/gofiber/fiber/v2"
-)
-
+import "github.com/gofiber/fiber/v2"
 func main() {
-	micro := fiber.New()
-	micro.Get("/doe", func(c *fiber.Ctx) error {
-		return c.SendStatus(fiber.StatusOK)
-	})
 	app := fiber.New()
-	app.Mount("/john", micro) // GET /john/doe -> 200 OK
-	log.Fatal(app.Listen(":3000"))
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World")
+	})
+	app.listen(":3000")
 }
