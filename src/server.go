95cf3225f8ca64e6f67d78e3fb2475c7445a799d
JinAru16 <70217158+JinAru16@users.noreply.github.com>
2021-12-04 00:57:29 +0900

    go 라우팅 연습

--- src/server.go
+++ src/server.go
@@ -1,10 +1,25 @@
 package main
-import "github.com/gofiber/fiber/v2"
-func main() {
-	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World")
-	})
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func hello(c *fiber.Ctx) error {
+	return c.SendString("welcome to Daily code buffer!")
+}
+
+func Routers(app *fiber.App) {
+	app.Get("/user", user.GetUsers)
+	app.Get("/user/:id", user.GetUsers)
+	app.Post("/user", user.SaveUser)
+	app.Delete("/user/:id", user.DeleteUSer)
+	app.Put("/user/:id", user.UpdateUSer)
+}
+
+func main() {
+	user.InitialMigration()
+	app := fiber.New()
+	app.Get("/", hello)
+	Routers(app)
 	app.Listen(":3000")
 }
